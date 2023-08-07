7e2b46a473d74c5e626e03b6ec0e9696c8cdd6ca
charlielovett <115426211+charlielovett@users.noreply.github.com>
2023-08-06 19:35:01 -0700

    Layout and NavBar

--- components/About.jsx
+++ components/About.jsx
@@ -3,10 +3,10 @@ import Image from 'next/image'
 
 const About = () => {
   return (
-    <div className='w-full md:h-screen p-2 flex items-center py-16'>
+    <div id='about' className='w-full md:h-screen p-2 flex items-center py-16'>
         <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
             <div className='col-span-2'>
-                <p className='uppercase text-xl tracking-widest text-{#5651e5]'>About</p>
+                <p className='uppercase text-xl tracking-widest text-[#5651e5]'>About</p>
                 <h2 className='py-4'>Who I Am</h2>
                 <p className='py-2 text-gray-600'>This is a paragraph explaining who I am. How I got into
                     programming, where I am in school, my interests, hobbies,
